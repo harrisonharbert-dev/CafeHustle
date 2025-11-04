8aac3034848cf48c70f900a4dcb7bc2448c74c76
HotRod03 <s4018156@student.rmit.edu.au>
2025-11-04 18:44:05 +1100

    Added Draggable Script for Objects

---
+++ Assets/Scripts/Interaction/Dragable.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dragable : MonoBehaviour
+{
+    Vector3 mousePositionOffset;
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDown()
+    {
+        mousePositionOffset = gameObject.transform.position - GetMouseWorldPosition();
+    }
+
+    private void OnMouseDrag()
+    {
+           transform.position = GetMouseWorldPosition() + mousePositionOffset;
+    }
+}
