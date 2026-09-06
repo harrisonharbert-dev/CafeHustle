dc2af029e5666219efd60a865c2f3798e2a2439d
toasty-seven <keanngooi@gmail.com>
2026-09-07 09:11:45 +1000

    Man u put the line in the wrong script man
    sad face

--- Assets/Scripts/Sound/AudioCaller.cs
+++ Assets/Scripts/Sound/AudioCaller.cs
@@ -4,6 +4,12 @@ public class AudioCaller : MonoBehaviour
 {
     public void callAudio(string id)
     {
+        if (SoundManager.instance == null)
+        {
+           Debug.LogWarning($"Missing SoundManager in scene when tried to call on {this}");
+           return;
+        }
+    ;
         SoundManager.instance.PlaySound2D(id);
     }
 }
--- Assets/Scripts/Sound/StoryCaller.cs
+++ Assets/Scripts/Sound/StoryCaller.cs
@@ -4,6 +4,11 @@ public class StoryCaller : MonoBehaviour
 {
     public void callStoryPoint(string id)
     {
+        if (StoryManager.instance == null)
+        {
+            Debug.LogWarning($"Missing StoryManager in scene when tried to call on {this}");
+            return;
+        }
         StoryManager.instance.SetStoryPoint(id);
     }
 }
